44a7f403b0f566e94c12f9834d8aa9e0ce2f9f3b
Jure Triglav <juretriglav@gmail.com>
2014-03-26 17:19:32 +0000

    Fix profile show

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,14 +2,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(resource)
-    profile_user_path(resource)
+    user_profile_path(resource)
   end
 
   def after_update_path_for(resource)
-    profile_user_path(resource)
+    user_profile_path(resource)
   end
 
   def after_sign_in_path_for(resource)
-    profile_user_path(resource)
+    user_profile_path(resource)
   end
 end
