454ef78fa01c5f5008d2c128bc1a645031f9fe42
Jure Triglav <juretriglav@gmail.com>
2014-03-28 15:12:11 +0000

    Limit to something reasonable

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def home
-    @tools = Tool.order('reproducibility_score DESC').all
+    @tools = Tool.order('reproducibility_score DESC').limit(100).all
     @tool = Tool.new
   end
 end
