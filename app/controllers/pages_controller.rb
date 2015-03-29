9edd108b5c81a3805fc600cbe4d8a163958d38b5
Jure Triglav <juretriglav@gmail.com>
2015-03-29 09:56:44 +0200

    Fix caching for pagination.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,13 @@
 class PagesController < ApplicationController
   def home
-    @tools = Tool.order('reproducibility_score DESC')
-                 .page(params[:page]).per_page(25)
+    @tools = Tool.order("reproducibility_score DESC").
+      page(page_params[:page]).per_page(25)
     @tool = Tool.new
+  end
+
+  private
+
+  def page_params
+    params.permit([:page])
   end
 end
